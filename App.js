e3cf298dcd8d2a5f5ae1c66f13c77ff21f14a9fd
MauroPachecoCano <mauro.pacheco@tecsup.edu.pe>
2021-06-28 19:01:43 -0500

    se implemento todo el laboratorio 5

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import "./css/app.css"
+import Cursos from "./componentes/cursos"
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  
+
+  return <Cursos  />
+}
+
+export default App
+
+
